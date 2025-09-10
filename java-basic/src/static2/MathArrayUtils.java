5310084dc0b958df929c7200ccf07e9596f0289c
gayo73 <163620291+gayo73@users.noreply.github.com>
2025-09-10 00:12:44 -0400

    [Sec7] static

---
+++ java-basic/src/static2/MathArrayUtils.java
@@ -0,0 +1,39 @@
+package static2;
+
+public class MathArrayUtils {
+    private MathArrayUtils() {
+        // private 인스턴스 생성을 막는다.
+    }
+
+    public static int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static double average(int[] array) {
+        return (double) sum(array) / array.length;
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
